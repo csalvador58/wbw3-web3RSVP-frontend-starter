7020b77e8294eb0b532003ded7ecb83fa391ec9c
Salvador <csalvador58@gmail.com>
2022-08-11 01:07:07 -0700

    Connected contract

---
+++ utils/connectContract.js
@@ -0,0 +1,26 @@
+import abiJSON from "./Web3RSVP.json";
+import { ethers } from "ethers";
+
+
+function connectContract() {
+    const contractAddress = "0x7E99bFB806700bcC17624E62a98d967ee99bA012";
+    const contractABI = abiJSON.abi;
+    let rsvpContract;
+    try {
+        const { ethereum } = window;
+
+        if (ethereum) {
+            //checking for eth object in the window
+            const provider = new.ethers.providers.Web3Provider(ethereum);
+            const signer = provider.getSigner();
+            rsvpContract = new ethers.Contract(contractAddress, contractABI, signer);
+        } else {
+            console.log("Ethereum object doesn't exist!")
+        } 
+    } catch (error) {
+        console.log("ERROR:", error);
+    }
+    return rsvpContract;
+}
+
+export default connectContract;
